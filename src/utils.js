4ac3536537f070de0350adbc1504a9ee80826e84
Nicolas M. Pardo <nicolasm@nikodermus.media>
2021-02-25 18:42:14 -0500

    feat(create test component): jest

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+import {
+    janKenPonFigures,
+    books,
+    cardOrder,
+    cardSigns,
+    characters,
+    movies,
+    numbers,
+    people,
+} from './data';
+
+// length
+export const maxItems = (arr, num) => {};
+
+// every
+export const canAllDrink = (arr) => {};
+
+// some
+export const canSomeoneDrink = (arr) => {};
+
+// filter
+export const characterFilter = () => {};
+
+// some;
+export const isIsogra = (str) => {};
+
+// rduce
+export const oddAndEe = (arr) => {};
+
+// fill
+export const countShe = (num) => {};
+
+// redce
+export const scoreAvrag = (arr) => {};
+
+// reduce + forEach
+export const moreCommonWord = (str, backList) => {};
+
+// Object.entries()
+export const findInObj = (obj, id) => {};
+
+// indexOf() + Math.random()
+export const janKenPo = (arr) => {};
+
+// pipe
+export const palindro = (str) => {};
+export const cesarCodex = (st, shift) => {};
+
+// Matrix
+export const matrixLtter = (leter, num) => {};
---
+++ test/utils.test.js
@@ -0,0 +1,18 @@
+import {
+    canAllDrink,
+    canSomeoneDrink,
+    cesarCodex,
+    characterFilter,
+    findInObj,
+    isIsogram,
+    matrixLetter,
+    maxItems,
+    moreCommonWord,
+    oddAndEven,
+    palindrome,
+    scoreAverage,
+} from '../src/utils';
+
+test('canAllDrink', () => {
+    expect(true).toBe(true);
+});
